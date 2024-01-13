9090dfb16fb8a4c8f83699d58c56bfc74b3d1d9e
Chaotix94 <chaotix94gaming@gmail.com>
2024-01-13 12:44:45 +0000

    moved price calculation to seperate function for multi-use

--- app/Http/Controllers/parkingController.php
+++ app/Http/Controllers/parkingController.php
@@ -13,15 +13,7 @@ class parkingController extends Controller
     {
         $input = json_decode($request->json()->all());
 
-        foreach (Price::all() as $price) {
-            // Check if given dates are between model dates, count days, provide price
-            if (Carbon::parse($input->arrival)->betweenIncluded($price->start, $price->end)) {
-                $days = $input->arrival->diffInDays($input->leaving);
-                $cost = $days * $price->cost;
-            };
-        };
-
-        return $cost;
+        return priceCalc($input->arrival, $input->leaving);
     }
 
     public function book(Request $request)
@@ -43,5 +35,39 @@ class parkingController extends Controller
     public function edit(Request $request)
     {
         $input = json_decode($request->json()->all());
+    }
+
+    public function priceCalc($arrival, $leaving)
+    {
+        $startDate = Carbon::parse($arrival);
+        $endDate = Carbon::parse($leaving);
+
+        $priceSet = Price::where('start', '>=', $startDate)
+            ->orderBy('date', 'asc')
+            ->first();
+
+        $prices = array(
+            'weekdayPrice' => $priceset->costday,
+            'weekendPrice' => $priceset->costend
+        );
+        
+        $weekdayCount = 0;
+        $weekendCount = 0;
+
+        for ($date = $startDate; $date->lte($endDate); $date->addDay()) {
+            // Check if the current day is a weekday (Monday to Friday)
+            if ($date->isWeekday()) {
+                $weekdayCount++;
+            } else {
+                $weekendCount++;
+            }
+        }
+
+        $costs = array(
+            'weekdayCost' => $weekdayCount * $prices['weekdayPrice'],
+            'weekendCost' => $weekendCount * $prices['weekendPrice']
+        )
+
+        return $costs;
     }
 }
